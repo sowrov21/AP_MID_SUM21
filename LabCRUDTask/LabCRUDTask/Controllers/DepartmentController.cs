3139f008736d6ffa6cfd688131a441814e2b8951
Sowrov21 <sowrov.it@gmail.com>
2021-06-18 18:10:58 +0600

    Department Insertion Done

--- LabCRUDTask/LabCRUDTask/Controllers/DepartmentController.cs
+++ LabCRUDTask/LabCRUDTask/Controllers/DepartmentController.cs
@@ -1,4 +1,5 @@
 ﻿using DatabaseCRUD.Models.Database;
+using LabCRUDTask.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,5 +17,45 @@ namespace LabCRUDTask.Controllers
             var departments = db.Departments.FetchAll();
             return View(departments);
         }
+
+        public ActionResult Create()
+        {
+            Department dpt = new Department();
+            return View(dpt);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Department dpt)
+        {
+            if (ModelState.IsValid)
+            {
+                Database db = new Database();
+                db.Departments.Insert(dpt); 
+                return RedirectToAction("Dashboard", "Login");
+            }
+            return View();
+        }
+
+
+
+        public ActionResult Edit(int id)
+        {
+            Database db = new Database();
+            var students = db.Departments.Get(id);
+            return View(students);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Department dpt)
+        {
+            //update to db
+            Database db = new Database();
+            db.Departments.Update(dpt);
+            return RedirectToAction("Index");
+        }
+
+
+
+
     }
 }
--- LabCRUDTask/LabCRUDTask/Models/Database/Departments.cs
+++ LabCRUDTask/LabCRUDTask/Models/Database/Departments.cs
@@ -34,5 +34,53 @@ namespace LabCRUDTask.Models.Database
             conn.Close();
             return departments;
         }
+
+        public void Insert(Department dpt)
+        {
+            string query = "Insert into Departments values(@name)";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@name", dpt.Name);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public Department Get(int id)
+        {
+            Department dpt = null;
+            string query = $"select * from departments Where Id={id}";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                dpt = new Department()
+                {
+                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                };
+            }
+            conn.Close();
+            return dpt;
+        }
+
+        public void Update(Department dpt)
+        {
+            string query = $"Update departments Set Name='{dpt.Name}' Where Id = {dpt.Id}";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public void Delete(int dpt)
+        {
+            string query = $"DELETE FROM Departments Where Id = {dpt}";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
     }
 }
